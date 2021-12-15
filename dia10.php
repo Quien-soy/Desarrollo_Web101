044ee16a295c733bb32589522fa42359b5832499
Primo Ticona <ticonad19@gmail.com>
2021-12-15 16:13:17 -0400

    cuarto commit

---
+++ dia10.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Ejerccios con PHP</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+    <?php
+        $x = 12;
+        $y = 34;
+        $z = $x + $y;
+        echo "<h1>El valor de z es $z</h1> "
+    
+    ?>
+</body>
+</html>
